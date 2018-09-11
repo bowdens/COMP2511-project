cfc80d1310a2ec0151afb5456209ffc3e7612f4a
bowdens <tomfbowden12@gmail.com>
2018-09-11 15:28:52 +1000

    added canMoveOnto method

--- ass2/DungeonProject/src/project/model/BoardEntity.java
+++ ass2/DungeonProject/src/project/model/BoardEntity.java
@@ -7,6 +7,9 @@ public abstract class BoardEntity {
 	private EntityType type;
 	private CollisionBehaviour collisionBehaviour;
 	
+	public abstract void canMoveOnto(BoardEntity entity);
+	
+	
 	public BoardEntity(int x, int y) {
 		this.x = x;
 		this.y = y;
