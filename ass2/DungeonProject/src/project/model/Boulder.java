b3fb7647f0c9cece7d71180355e14d9b9a287e9d
Jonathan Williams <z5162987@ad.unsw.edu.au>
2018-09-13 13:46:35 +1000

    Updated boulder's canMoveOnto method

--- ass2/DungeonProject/src/project/model/Boulder.java
+++ ass2/DungeonProject/src/project/model/Boulder.java
@@ -10,10 +10,39 @@ public class Boulder extends BoardEntity {
 
 	@Override
 	public boolean canMoveOnto(BoardEntity entity) {
-		// players can move onto it, noone else
-		if (entity instanceof Player) {
+		/if entity isn't a player return false
+		if(!(entity instanceof Player)) {
+			return false;
+		}
+		int newX;
+		int newY;
+		//get the coords of the block which the boulder will be moving onto
+		Direction entDirection = entity.getDirection();
+		switch (entDirection) {
+			case UP:
+				newX = this.getX();
+				newY = this.getY()-1;
+				break;
+			case DOWN:
+				newX = this.getX();
+				newY = this.getY()+1;
+				break;
+			case LEFT:
+				newX = this.getX()-1;
+				newY = this.getY();
+				break;
+			case RIGHT:
+				newX = this.getX()+1;
+				newY = this.getY();
+				break;
+		}
+		
+		//get object that is on this coordinate
+		BoardEntity movinOnto = board.getEntityAt(newX, newY);
+	    //check if the boulder can move onto this object
+		if(movinOnto.canMoveOnto(this, board)) {
 			return true;
-		} else {
+		}else {
 			return false;
 		}
 	}
