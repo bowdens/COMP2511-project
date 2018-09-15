9d25dc692faf81cd438eca5c73f499b5868ccb15
Jonathan Williams <z5162987@ad.unsw.edu.au>
2018-09-15 20:44:58 +1000

    Implemented potions

--- ass2/DungeonProject/src/project/model/collisionBehaviours/PickUpInvincibilityPotion.java
+++ ass2/DungeonProject/src/project/model/collisionBehaviours/PickUpInvincibilityPotion.java
@@ -10,7 +10,12 @@ public class PickUpInvincibilityPotion implements CollisionBehaviour {
 	@Override
 	public void collide(Board board, BoardEntity mover, BoardEntity me) {
 		if (mover instanceof Player) {
-			((Player) mover).addInvincibility(((Player)mover).potionSpan);
+			//if the player is already hovering, then extend the hover time
+			if(((Player) mover).isInvincible()){
+				((Player) mover).addInvincibility(((Player)mover).potionSpan);
+			}else {
+				((Player) mover).setInvincible(true);
+			}
 			board.removeBoardEntity(me);
 		} else {
 			return;
