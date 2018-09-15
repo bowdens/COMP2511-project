53ca2db42f08779a177e7fb6883ec767eeac187b
Jonathan Williams <z5162987@ad.unsw.edu.au>
2018-09-15 17:56:29 +1000

    Done collide with enemy

--- ass2/DungeonProject/src/project/model/collisionBehaviours/CollideWithEnemy.java
+++ ass2/DungeonProject/src/project/model/collisionBehaviours/CollideWithEnemy.java
@@ -3,12 +3,22 @@ package project.model.collisionBehaviours;
 import project.model.Board;
 import project.model.BoardEntity;
 import project.model.CollisionBehaviour;
+import project.model.Player;
+
 
 public class CollideWithEnemy implements CollisionBehaviour {
 
 	@Override
 	public void collide(Board board, BoardEntity mover, BoardEntity me) {
-		// TODO: end the game unless the player has an invincibility potion or a sword
+		if(((Player) mover).isInvincible()){
+			board.removeBoardEntity(me);
+		}else if(((Player) mover).getSwords() > 0 ) {
+			board.removeBoardEntity(me);
+			((Player) mover).addSwords(-1);
+		}else {
+			board.endGame();
+		}
+		
 	}
 
 }
