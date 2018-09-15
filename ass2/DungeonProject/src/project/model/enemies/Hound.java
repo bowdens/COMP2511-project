0021f5fcaa42c03e7227f66ab4c5f9511f113666
VishaalVageshwari <z5161415@student.unsw.edu.au>
2018-09-16 02:59:24 +1000

    Small change

--- ass2/DungeonProject/src/project/model/enemies/Hound.java
+++ ass2/DungeonProject/src/project/model/enemies/Hound.java
@@ -1,5 +1,13 @@
 package project.model.enemies;
 
-public class Hound {
+import project.model.Enemy;
+
+public class Hound extends Enemy {
+
+	private static final long serialVersionUID = 1L;
+
+	public Hound(int x, int y) {
+		super(x, y);
+	}
 
 }
