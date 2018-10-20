3cb61e2cfcb4445a08e4cf4890892745c80364af
NickChanghao <r27290ch@gmail.com>
2018-10-20 23:38:52 +1100

    Add files via upload
    change movements

--- ass2/DungeonProject/src/project/model/collisionBehaviours/CollideWithPlayer.java
+++ ass2/DungeonProject/src/project/model/collisionBehaviours/CollideWithPlayer.java
@@ -11,24 +11,24 @@ public class CollideWithPlayer implements CollisionBehaviour {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @pre me must be a player
+	 * @pre me should be a player
 	 */
 	@Override
 	public void collide(Board board, BoardEntity mover, BoardEntity me) {
-		// if the mover is an enemy, kill the player unless they are invincible
-		// cast me to a player ... if its not do nothing
+		// when the mover is an enemy, kill the player unless they are invincible
+		// cast me to a player if its not do nothing
 		if (!(me instanceof Player)) {
 			return;
 		}
 		
-		Player player = (Player) me;
+		Player player1 = (Player) me;
 		if (mover instanceof Enemy) {
-			if (player.isInvincible()) {
-				// kill the mover (the enemy)
+			if (player1.isInvincible()) {
+				// kill the mover --- the enemy
 				board.removeBoardEntity(mover);
-			} else if (player.getSwords() > 0) {
+			} else if (player1.getSwords() > 0) {
 				board.removeBoardEntity(mover);
-				player.addSwords(-1);
+				player1.addSwords(-1);
 			} else {
 				board.endGame();
 			}
