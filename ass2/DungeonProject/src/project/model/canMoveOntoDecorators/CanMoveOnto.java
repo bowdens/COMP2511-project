90adbd37df61bee8d5ad04e90b9db754530d42f7
bowdens <tomfbowden12@gmail.com>
2019-02-21 18:36:08 +1100

    fixed folders

--- ass2/DungeonProject/src/project/model/canMoveOntoDecorators/CanMoveOnto.java
+++
@@ -1,8 +0,0 @@
-package project.model.canMoveOntoDecorators;
-
-import project.model.Board;
-import project.model.BoardEntity;
-
-public interface CanMoveOnto {
-	public boolean canMoveOnto(Board board, BoardEntity mover);
-}
